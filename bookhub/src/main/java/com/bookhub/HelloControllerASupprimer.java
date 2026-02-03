7612d3e214d31d82ffc7e29cd57adc1e5948874d
solena8 <solena_ananda@hotmail.com>
2026-02-03 16:16:54 +0100

    initialisation du porjet spring

--- bookhub/src/main/java/com/bookhub/HelloControllerASupprimer.java
+++ bookhub/src/main/java/com/bookhub/HelloControllerASupprimer.java
@@ -1,4 +1,14 @@
 package com.bookhub;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloControllerASupprimer {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
 }
