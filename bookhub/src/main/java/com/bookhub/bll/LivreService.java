77bfda7dbfba5d52ca88f4ebade06760f047e87e
DearrLo <viellorette@gmail.com>
2026-02-05 13:16:51 +0100

    LivreService + LivreServiceImpl

---
+++ bookhub/src/main/java/com/bookhub/bll/LivreService.java
@@ -0,0 +1,19 @@
+package com.bookhub.bll;
+
+import java.util.List;
+
+import com.bookhub.bo.Livre;
+
+public interface LivreService {
+	List<Livre> afficherLivres(); // TODO à modif avec pagination
+	
+	Livre afficherDetailLivre(Integer id);
+	Livre ajouter(Livre livre);
+	Livre modifier(Livre livre);
+	void supprimer(Integer id);
+	
+	List<Livre> chargerLivreParCategorie (Integer idCategorie);
+	
+	// Retourne livre dispo si stock > 0
+	List<Livre> chargerLivreParDisponibilite();
+}
---
+++ bookhub/src/main/java/com/bookhub/bll/LivreServiceImpl.java
@@ -0,0 +1,55 @@
+package com.bookhub.bll;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.bookhub.bo.Livre;
+import com.bookhub.dal.LivreRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class LivreServiceImpl implements LivreService {
+
+	private final LivreRepository livreRepository;
+
+	@Override
+	public List<Livre> afficherLivres() {
+		return livreRepository.findAll();
+	}
+
+	@Override
+	public Livre afficherDetailLivre(Integer id) {
+		return livreRepository	.findById(id)
+								.orElse(null);
+	}
+
+	@Override
+	public Livre ajouter(Livre livre) {
+		return livreRepository.save(livre);
+	}
+
+	@Override
+	public Livre modifier(Livre livre) {
+		return livreRepository.save(livre);
+	}
+
+	@Override
+	public void supprimer(Integer id) {
+		livreRepository.deleteById(id);
+	}
+
+	@Override
+	public List<Livre> chargerLivreParCategorie(Integer idCategorie) {
+		return livreRepository.findByCategorie_Id(idCategorie);
+	}
+
+	@Override
+	public List<Livre> chargerLivreParDisponibilite() {
+
+		// Un livre est disponible si stock > 0
+		return livreRepository.findByStockGreaterThan(0);
+	}
+}
