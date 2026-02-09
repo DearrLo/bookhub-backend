1930b5a2350cad62e1924f42bc4b41ccdba6b49c
solena8 <solena_ananda@hotmail.com>
2026-02-09 14:48:17 +0100

    fixtures ok

--- bookhub/src/main/resources/requetes_fixtures.sql
+++ bookhub/src/main/resources/requetes_fixtures.sql
@@ -1,6 +1,17 @@
 USE BOOKHUB;
 
-DELETE FROM BOOK;
+DELETE
+FROM bookhub_loan;
+DELETE
+FROM reservation;
+DELETE
+FROM review;
+DELETE
+FROM book;
+DELETE
+FROM bookhub_user;
+DELETE
+FROM category;
 
 INSERT INTO category (label)
 VALUES ('Jeunesse'),
@@ -55,5 +66,42 @@ VALUES ('9782070408504', 'Le Petit Prince', 'Antoine de Saint-Exupéry', 1, 10,
         'Dans un monde totalitaire, Winston Smith tente de résister à l''oppression de Big Brother.',
         'https://covers.openlibrary.org/b/isbn/9780451524935-L.jpg', GETDATE());
 
-SELECT * FROM book;
-SELECT * FROM category;
+INSERT INTO bookhub_user(ROLE, NAME, PSEUDONYM, SURNAME, EMAIL, PASSWORD)
+VALUES ('lecteur', 'Jean', 'jeannot31', 'André', 'jeannot31@email.com', 'Pompompom1!'),
+       ('admin', 'Nour', 'nourette', 'Martin', 'nour567@email.com', 'Pompompom2!'),
+       ('bibliothecaire', 'Soléna', 'soso', 'Toussaint', 'solena@email.com', 'Pompompom3!');
+
+INSERT INTO reservation(BOOK_ID, REQUEST_DATE, RESERVATION_STATUS, USER_ID)
+VALUES (1, GETDATE(), 'EN_ATTENTE', 'jeannot31@email.com'),
+       (4, GETDATE(), 'EN_ATTENTE', 'jeannot31@email.com'),
+       (10, GETDATE(), 'EN_ATTENTE', 'jeannot31@email.com');
+
+INSERT INTO bookhub_loan(book_id, date_to_return, returned_date, loan_date, loan_status, user_id)
+VALUES (3, GETDATE() + 14, null, GETDATE(), 'EMPRUNTE', 'c'),
+       (7, GETDATE() + 14, null, GETDATE(), 'EMPRUNTE', 'jeannot31@email.com'),
+       (8, GETDATE(), GETDATE(), GETDATE() - 14, 'RENDU', 'jeannot31@email.com');
+
+INSERT INTO review(book_id, rating, created_at, commentary, user_id)
+VALUES (1, 4, GETDATE() - 5, 'Pas mal', 'jeannot31@email.com'),
+       (2, 5, GETDATE() - 5, 'Génial', 'jeannot31@email.com'),
+       (3, 1, GETDATE() - 5, 'Pas trop aimé', 'jeannot31@email.com');
+
+
+SELECT *
+FROM book;
+
+SELECT *
+FROM category;
+
+SELECT *
+FROM bookhub_user;
+
+SELECT *
+FROM reservation;
+
+SELECT *
+FROM bookhub_loan;
+
+SELECT *
+FROM review;
+
